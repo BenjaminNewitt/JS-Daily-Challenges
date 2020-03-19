04a43d0aa331d412048ed13d3e918b9b826bd7f7
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-03-19 09:21:43 -0600

    Is it Time for Milk and Cookies?

--- js-daily-challenges.js
+++ js-daily-challenges.js
@@ -1,1 +1,10 @@
 // March 19, 2020
+function timeForMilkAndCookies(date) {
+  let month = date.getMonth();
+  let day = date.getDate();
+
+  if (month == 11 && day == 24) {
+    return true;
+  }
+  return false;
+}
